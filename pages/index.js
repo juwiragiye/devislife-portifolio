bd869079f7e660386a66d8a05dd33ea571ff6e55
juwiragiye <uwiragiyejanvi@gmail.com>
2022-01-13 06:46:21 -0500

    project bootstrap

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Container, Box } from '@chakra-ui/react'
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg">
+        Hello, I&apos;m full-stack developer born in Burundi, based in Canada!
+      </Box>
+    </Container>
+  )
+}
+
+export default Page
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracktSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabsWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
